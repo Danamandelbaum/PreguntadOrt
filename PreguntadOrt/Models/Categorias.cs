6d33768e2edf978de0d47d273b35380295bad028
UMA CATARINA GOTFRYD <47805869@ort>
2023-08-08 08:48:24 -0300

    Models

---
+++ PreguntadOrt/Models/Categorias.cs
@@ -0,0 +1,6 @@
+namespace PreguntadOrt.Models;
+public class Categorias
+{
+    public int IdCategoria {get;private set;}
+    public string Nombre {get;private set;}
+}
---
+++ PreguntadOrt/Models/Respuestas.cs
@@ -0,0 +1,9 @@
+namespace PreguntadOrt.Models;
+public class Respuestas
+{
+    public int IdRespuesta {get;private set;}
+    public int IdPregunta {get;private set;}
+    public int Opcion {get;private set;}
+    public string Contenido {get;private set;}
+    public bool Correcta {get;private set;}
+}
